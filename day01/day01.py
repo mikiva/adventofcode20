8562333bd34f68c66935055953c289953d192e73
Mikael Ivarsson <mikael.ivarsson@lm.se>
2020-12-01 14:33:28 +0100

    de-import

--- day01/day01.py
+++ day01/day01.py
@@ -1,9 +1,8 @@
 from itertools import combinations
-import sys
 
 terms = [2, 3]
-
 lines = []
+
 with open("input.txt", "r") as inp:
     for line in inp:
         lines.append(int(line))
@@ -12,11 +11,10 @@ def multiply(factors):
     res = 1
     for f in factors:
         res = res * f
-
     return res
 
-for term in terms:
 
+for term in terms:
     for c in combinations(lines, term):
         if sum(c) == 2020:
             print(f'{term}: {multiply(c)}')
