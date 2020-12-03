de3aadbcfd3dd64dfa286e0f817cee7b408bca91
Mikael Ivarsson <mikael.ivarsson@lm.se>
2020-12-03 10:34:08 +0100

    some cleaning

--- day03/day03.py
+++ day03/day03.py
@@ -6,27 +6,22 @@ with open("input.txt", "r") as inp:
     x = 0
     for line in inp:
         row = line.strip("\n")
-        if row[x%len(row)] == "#":
-            count = count +1
+        if row[x % len(row)] == "#":
+            count = count + 1
         x += 3
         tree_map.append(row)
 
-
-slopes = [(1,1), (3,1), (5,1),(7,1), (1,2)]
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 slope_trees = 1
-
 for slope in slopes:
-    c = 0
-    curr_x = 0
-    curr_y = 0
-    while curr_y < len(tree_map):
-        row = tree_map[curr_y]
-        if row[curr_x%len(row)] == "#":
+    c, x, y = 0, 0, 0
+    while y < len(tree_map):
+        row = tree_map[y]
+        if row[x % len(row)] == "#":
             c += 1
-        curr_x += slope[0]
-        curr_y += slope[1]
+        x += slope[0]
+        y += slope[1]
     slope_trees = slope_trees * c
 
 print("a", count)
 print("b", slope_trees)
-
