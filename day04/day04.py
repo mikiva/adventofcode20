d86bcc68ab96d58d0979ff8c503c8be67802f499
Mikael Ivarsson <mikael.ivarsson@lm.se>
2020-12-04 07:46:33 +0100

    refactor

--- day04/day04.py
+++ day04/day04.py
@@ -2,31 +2,19 @@
 
 fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 opt = "cid"
-
 pp = []
-
 with open("input.txt", "r") as inp:
-    p = ""
-    for line in inp:
-        p  += line
-    pp = p.split("\n\n")
+    pp = [x.replace("\n", " ") for x in inp.read().split("\n\n")]
 
 def check_passport(passport):
     valid = True
-    for f in fields:
-        if f not in [x[:3] for x in passport.split()]:
-            valid = False
+    f = [f for f in fields if f not in [x[:3] for x in passport.split()]]
+    return len(f) == 0
 
-    return valid
 
-def byr(p):
-    return int(p) >= 1920 and int(p) <= 2020
+def interval(p, miny, maxy):
+    return int(p) >= miny and int(p) <= maxy
 
-def iyr(p):
-    return int(p) >= 2010 and int(p) <= 2020
-
-def eyr(p):
-    return int(p) >= 2020 and int(p) <= 2030
 def hgt(p):
     if p[-2:] == "cm":
         return int(p[:3]) >= 150 and int(p[:3]) <= 193
@@ -42,18 +30,16 @@ def ecl(p):
 def pid(p):
     return len(str(p)) == 9 and int(p) #Throws if not integer
 
-
-
 def check_passport_for_real(passport):
     valid = True
     for field in [x.split(":") for x in passport.split()]:
         try:
             if field[0] == "byr":
-                valid = byr(field[1])
+                valid = interval(field[1], 1920, 2020)
             elif field[0] == "iyr":
-                valid = iyr(field[1])
+                valid = interval(field[1], 2010, 2020)
             elif field[0] == "eyr":
-                valid = eyr(field[1])
+                valid = interval(field[1], 2020, 2030)
             elif field[0] == "hgt":
                 valid = hgt(field[1])
             elif field[0] == "hcl":
@@ -72,8 +58,6 @@ valid = []
 valid_for_real = []
 
 for p in pp:
-    p = p.replace("\n", " ")
-    
     if check_passport(p):
         valid.append(p)
         if check_passport_for_real(p):
