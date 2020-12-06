3c4cc5699b23269f7987b7ee7c92c7308bf33730
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-06 10:56:23 +0100

    day06

---
+++ day06/day06.py
@@ -0,0 +1,28 @@
+answers = []
+with open("input.txt", "r") as inp:
+    answers = inp.read().split("\n\n")
+    #print(answers)
+    
+def anyone():
+    yes = 0
+    
+    for ans in [x.replace("\n", "") for x in answers]:
+        c = set()
+        for a in ans:
+            c.add(a)
+        yes += len(c)
+    return yes
+
+def everyone():
+    yes = 0
+    tested = set()
+    visited = list()
+
+    for group in answers:
+        ans = [set(x) for x in group.split("\n")]
+        yes += len(ans[0].intersection(*ans))
+            
+    return yes
+
+print("a", anyone())
+print("b", everyone())
