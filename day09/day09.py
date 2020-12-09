04c36426c91ef6af415400ea8e3bcc9421a71200
Mikael Ivarsson <mikael.ivarsson@lm.se>
2020-12-09 16:52:22 +0100

    justeringar

--- day09/day09.py
+++ day09/day09.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from itertools import combinations
-
-preamble = 25
+from sys import argv
+preamble = int(argv[1])
 lines = [int(x) for x in open("input.txt")]
 
 
@@ -23,10 +23,13 @@ def find_invalid():
 
 def try_weakness(pre, check):
     weaklist = []
+    
     for p in pre:
         weaklist.append(p)
         if sum(weaklist) == check:
             return True,  weaklist
+        elif sum(weaklist) > check:
+            break
     return False, weaklist
 
 
@@ -35,20 +38,26 @@ def find_weakness(invalid):
     for l in range(preamble, len(lines)):
         pre = lines[l-preamble:l]
         found, weak = try_weakness(pre, invalid)
-        if found > 0:
+        if found:
             weakness = min(weak) + max(weak)
             break
 
-    return {"a": invalid, "b": weakness}
+    return weakness#{"a": invalid, "b": weakness}
 
+    
 
 def run():
-    print(f'{find_weakness(find_invalid())}')
-
+    invalid = find_invalid()
+    weakness = find_weakness(invalid)
+    #print(f'{find_weakness(invalid)}')
+    return invalid, weakness
 
 start_time = datetime.now()
-run()
+inv, wea = run()
 end_time = datetime.now()
 tot_time = (end_time - start_time).microseconds / 1000
 
+
+print(f'A: {inv}')
+print(f'B: {wea}')
 print(f'Time: {tot_time} ms')
