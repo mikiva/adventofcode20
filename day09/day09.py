6a892d2a45dbecf61e58cae402d5657aaa13ae09
Mikael Ivarsson <mikael.ivarsson@lm.se>
2020-12-09 13:03:05 +0100

    timing and cleaning

--- day09/day09.py
+++ day09/day09.py
@@ -1,43 +1,54 @@
+from datetime import datetime
 from itertools import combinations
+
 preamble = 25
-lines = []
-with open("input.txt") as inp:
-    lines = [int(x) for x in inp.read().split()]
+lines = [int(x) for x in open("input.txt")]
+
 
 def check_sum(pre, check, terms):
-    for term in terms:
-        if term < 2: continue
-        for c in combinations(pre, term):
-            if sum(c) == check:
-                return True
-
+    for c in combinations(pre, terms):
+        if sum(c) == check:
+            return True
     return False
 
-invalid = -1
-for l in range(preamble, len(lines)):
-    pre, line = lines[l-preamble:l], lines[l]
-    valid= check_sum(pre, lines[l], [2])
-    if not valid:
-        invalid = line
-        mi, ma = min(pre), max(pre)
-        break
 
-def find_weakness(pre, check):
+def find_invalid():
+    for l in range(preamble, len(lines)):
+        pre, line = lines[l-preamble:l], lines[l]
+        valid = check_sum(pre, lines[l], 2)
+        if not valid:
+            return line
+    return -1
+
+
+def try_weakness(pre, check):
     weaklist = []
     for p in pre:
         weaklist.append(p)
         if sum(weaklist) == check:
             return True,  weaklist
-    return False,weaklist
-
-weakness = -1
-for l in range(preamble, len(lines)):
-    pre = lines[l-preamble:l]
-    found , weak = find_weakness(pre, invalid)
-    if found > 0:
-        weakness = min(weak) + max(weak)
-        break
+    return False, weaklist
 
 
-print("a", invalid)
-print("b", weakness)
+def find_weakness(invalid):
+    weakness = -1
+    for l in range(preamble, len(lines)):
+        pre = lines[l-preamble:l]
+        found, weak = try_weakness(pre, invalid)
+        if found > 0:
+            weakness = min(weak) + max(weak)
+            break
+
+    return {"a": invalid, "b": weakness}
+
+
+def run():
+    print(f'{find_weakness(find_invalid())}')
+
+
+start_time = datetime.now()
+run()
+end_time = datetime.now()
+tot_time = (end_time - start_time).microseconds / 1000
+
+print(f'Time: {tot_time} ms')
