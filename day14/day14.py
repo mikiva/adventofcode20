02fc286da18eacb4123e78dc519c0d46500c998a
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-14 16:11:41 +0100

    day14 prep

---
+++ day14/day14.py
@@ -0,0 +1,20 @@
+
+
+prog = {}
+
+def read_input():
+    with open("ex.txt") as inp:
+        pass
+
+
+
+
+
+def solve():
+    read_input()
+    print("hello")
+
+
+
+if __name__ == "__main__":
+    solve()
