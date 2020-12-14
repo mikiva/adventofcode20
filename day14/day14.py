cd1eb3dba8e81746d6d45faaa5c8db8c6dca0df8
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-14 22:27:05 +0100

    day14 part1

--- day14/day14.py
+++ day14/day14.py
@@ -1,20 +1,38 @@
 
+def value_plus_mask(val, mask):
+    val = [x for x in str(val)]
+    for idx, m in enumerate(mask):
+        if m != "X":
+            val[idx] = m
+    val = "".join(val)
 
-prog = {}
+    return val
 
 def read_input():
-    with open("ex.txt") as inp:
-        pass
-
-
-
-
+    prog = {}
+    mask = ""
+    with open("input.txt") as inp:
+        for line in inp:
+            if line.startswith("mask"):
+                mask = line.replace(" ", "").split("=")[1].strip("\n")
+            else:
+                key, value = line.replace(" ", "").split("=")
+                value = "%036d" % int(bin(int(value))[2:])
+                
+                value = value_plus_mask(value, mask)
+                
+                prog[key] = value
+    count = 0
+    for v,k in prog.items():
+        count += int(k,2)
+    
+    print(f'A: {count}')
 
 def solve():
     read_input()
-    print("hello")
 
 
 
 if __name__ == "__main__":
     solve()
+    
