93ada3e01fe882db68831052c3ffbc2e8523b553
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-17 16:10:23 +0100

    day17 part 1

--- day17/day17.py
+++ day17/day17.py
@@ -1,24 +1,16 @@
-import numpy as np
-
 from collections import defaultdict
 
 def read_input():
     
-    input=".#.\n..#\n###"
-    #input = open("input").read()
-    #with open("input") as inp:
-    #    input = inp.read()
-    print(input)
-
+    #input=".#.\n..#\n###"
+    input = open("input").read()
     grid = []
     for row in input.split("\n"):
         r = []
         for cell in row:
             r.append(cell)
-
         grid.append(r)
     return grid
-
 
 def parse_lines(grid):
     active = set()
@@ -28,27 +20,35 @@ def parse_lines(grid):
             if vv == "#":
                 active.add((i,ii, 0))
 
-    print(active)
     return active
 
 
+def cycle(active):
 
+    new_active = defaultdict(int)
 
+    for x,y,z in active:
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                for dz in [-1, 0, 1]:
+                    if dx == 0 and dy == 0 and dz == 0:
+                        continue
+                    new_active[(x+dx, y+dy, z+dz)] += 1
 
+    current_active = {p for p, v in new_active.items() if v == 3 or (v == 2 and p in active) }
+    return current_active
 
+def part_1(lines):
+    for i in range(6):
+        lines = cycle(lines)
+    return len(lines)
+
+def solve(lines):
+    print(f'A: {part_1(lines)}')
 
 if __name__ == "__main__":
     lines = parse_lines(read_input())
-    #print(lines)
-   # print(part1(grid))
-    print(lines)
-    pass
+
+    solve(lines)
 
 
-"""
-Lägg in alla aktiva i ett set. 
-För varje cykel, kolla alla aktiva och titta på varje granne med nästlade for-loopar, räkna hur många av dessa som återfinns i befintligt set.
-Lägg till varje punkt i nytt set.
-Returnera endast de punkter som har grannar enligt reglerna
-
-"""
