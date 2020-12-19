e46f8404e2b2a9d0d3620c7c27e3b10f945dc16e
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-19 16:01:32 +0100

    day19 part 2

--- day19/day19.py
+++ day19/day19.py
@@ -1,34 +1,50 @@
 import re
 
+part2 = False
+rules = dict()
+
 def read_input(file):
-    rules, messages = open(file).read().split("\n\n")
+    global rules
+    rules_raw, messages = open(file).read().split("\n\n")
 
     mess = []
     rule_list = dict()
 
-    for rule in rules.split("\n"):
+    for rule in rules_raw.split("\n"):
         id, content = rule.split(":")
         rule_list[id] = content.strip().split()
     for message in messages.split("\n"):
         mess.append(message)
-    return rule_list, mess
+
+    rules = rule_list
+    return mess
 
 
-def get_regex_part(part, rules):
+def get_regex_part(part):
     if part.startswith('"'):
         return part[1]
     elif part == '|':
         return '|'
     else: 
-        return '(' + build_regexp(part, rules) + ')'
+        r = build_regexp(part)
+        if len(r) == 1: return r
+        return '(' + r + ')'
 
-def build_regexp(number, rules):
-    return ''.join(get_regex_part(reg, rules) for reg in rules[number])
+def build_regexp(number):
+
+    if part2:
+         if number == '8':
+             return '(' + build_regexp("42") + ')+'
+         elif number == '11':
+             r = '|'.join(get_regex_part('42')*n + get_regex_part('31')*n for n in range(1,20))
+             return r
+
+    return ''.join(get_regex_part(reg) for reg in rules[number])
 
 
-def part_1(messages, rules):
+def run(messages):
 
-    regexp = build_regexp('0', rules)
+    regexp = build_regexp('0')
     count = 0
     for message in messages:
         if re.match('^' + regexp + '$', message):
@@ -36,8 +52,12 @@ def part_1(messages, rules):
     return count
 
 def solve(file):
-    rules, messages = read_input(file)
-    print(f'A: {part_1(messages, rules)}')
+    global part2
+    messages = read_input(file)
+    print(f'A: {run(messages)}')
+
+    part2 = True
+    print(f'B: {run(messages)}')
 
 if __name__ == "__main__":
     solve("input")
