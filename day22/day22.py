6acc7b1470cfd9d7e10a16cf82daa4a9a7b0b56d
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-22 11:13:39 +0100

    day22 part 2

--- day22/day22.py
+++ day22/day22.py
@@ -1,39 +1,66 @@
+from copy import deepcopy
+from collections import defaultdict, deque
+
 
 def read_input(filename):
     p1, p2 = open(filename).read().split("\n\n")
-    p1 = [int(x) for x in p1.split("\n")[1:]]
-    p2 = [int(x) for x in p2.split("\n")[1:]]
-    return p1,p2
+
+    pd1, pd2 = [],[]
+
+    for p in p1.split("\n"):
+        if p.startswith("Player"):
+            continue
+        pd1.append(int(p))
+    for p in p2.split("\n"):
+        if p.startswith("Player"):
+            continue
+        pd2.append(int(p))
+
+    return pd1, pd2
+
+def play(p1, p2, part2=False):
+    seen = set()
+    while p1 and p2:
+        seen_key = (tuple(p1), tuple(p2))
+        if part2 and seen_key in seen:
+            return True, p1
+
+        seen.add((seen_key))
+
+        p1card,p2card = p1.pop(0), p2.pop(0)
+
+        if part2 and (len(p1) >= p1card and len(p2) >= p2card):
+            new_p1card = p1[:p1card]
+            new_p2card = p2[:p2card]
+            p1_winner,_ = play(new_p1card, new_p2card, part2)
+        else:
+            p1_winner = p1card > p2card
+
+        if p1_winner:
+            p1.append(p1card)
+            p1.append(p2card)
+        else:
+            p2.append(p2card)
+            p2.append(p1card)
 
 
-def play(p1, p2):
+    return (True, p1) if p1 else (False, p2)
 
-    while len(p1) > 0  and len(p2) > 0:
-        p1play = p1.pop(0)
-        p2play = p2.pop(0)
+def play_game(p1, p2, part2=False):
+    player, deck=play(p1, p2, part2)
+    count=0
+    for i, p in enumerate(deck):
+        count += p * (len(deck) - i)
+    return player, count
 
-        if p1play > p2play:
-            p1.append(p1play)
-            p1.append(p2play)
-        elif p1play < p2play:
-            p2.append(p2play)
-            p2.append(p1play)
 
-    if len(p1) == 0:
-        return p2
-    return p2
- 
-
-def part_1(p1,p2):
-    winner = play(p1,p2)
-    count = 0
-    for i,p in enumerate(winner[::-1]):
-        count += p * (i +1)
-    return count
 def solve(filename):
     p1, p2 = read_input(filename)
 
-    print(f'A: {part_1(p1,p2)}')
+    winner, score = play_game(deepcopy(p1),deepcopy(p2))
+    print(f'A: Winner player: {1 if winner else 2} - {score}')
+    winner, score = play_game(deepcopy(p1),deepcopy(p2), True)
+    print(f'B: Winner player: {1 if winner else 2} - {score}')
 
 
 if __name__ == "__main__":
