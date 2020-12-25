0af8e5df53d6cbf9125ea513001866930285a819
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-12-25 12:47:59 +0100

    day23 part 2

--- day23/day23.py
+++ day23/day23.py
@@ -1,18 +1,27 @@
 from collections import deque
-
+from copy import deepcopy
 #inp = 389125467
 inp = 974618352
 input = deque(int(x) for x in str(inp))
 
+number_of_mugs = 10 ** 6
 
+def fill_mug_circle(mugs, part2=False):
+    max_mugs = max(mugs)
+    new_mugs = [-1] * (number_of_mugs+1)
+    for m in range(len(mugs)-1):
+        new_mugs[mugs[m]] = mugs[m+1]
+    new_mugs[mugs[-1]] = max(mugs)+1
+    for i in range(max(mugs)+1, number_of_mugs):
+        new_mugs[i] = i+1
+    new_mugs[number_of_mugs] = mugs[0]
+    return new_mugs
 def rotate_mugs(curr, r, mugs):
     while True:
         if mugs[r] == curr:
             return mugs
         else:
             mugs.rotate(1)
-
-
 def play(mugs, rounds):
     c = 0
     put_aside = []
@@ -53,24 +62,53 @@ def play(mugs, rounds):
         mugs = rotate_mugs(current, idx, mugs)
     return mugs
 
+def play2(mugs, current):
+    put_aside = []
+    put_aside.append(mugs[current])
+    for _ in range(2):
+        put_aside.append(mugs[put_aside[-1]])
+
+    
+    mugs[current] = mugs[put_aside[-1]]
+
+    target = current - 1
+
+    while target in put_aside or target == 0:
+        target -= 1
+        if target < 1:
+            target = number_of_mugs
+
+        
+    mugs[put_aside[-1]] = mugs[target]
+    mugs[target] = put_aside[0]
+
+    return mugs, mugs[current]
 
 def part_1():
     rounds = 100
-    p = play(input, rounds)
+    p = play(deepcopy(input), rounds)
     idx = p.index(1)
     ans = []
     for _ in range(len(p)):
         ans.append(p[idx % len(p)])
         idx += 1
     mugs = rotate_mugs(1, 0, p)
-    mugs = filter(lambda x: x != 1, mugs)
+    mugs.remove(1)
     mugs_str = "".join(map(str, mugs))
     return mugs_str
 
-
+def part_2():
+    mugs = fill_mug_circle(list(input))
+    rounds = 10 ** 7
+    current = input[0]
+    for _ in range(rounds):
+        mugs, current = play2(mugs, current)
+    first_mug = mugs[1]
+    second_mug = mugs[first_mug]
+    return first_mug * second_mug
 def solve():
     print(f'A: {part_1()}')
-
+    print(f'B: {part_2()}')
 
 if __name__ == "__main__":
     solve()
